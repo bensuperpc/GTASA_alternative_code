4de7418a8df3fbf482e376dfa736ad58b65b4c9f
Bensuperpc <bensuperpc@gmail.com>
2021-07-07 00:19:17 +0200

    Improve builder scripts
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- builder/functions/cmake_fn.sh
+++ builder/functions/cmake_fn.sh
@@ -3,16 +3,17 @@ set -euo pipefail
 
 cmake_build () {
     local image=$1
+    local build_file=build-${image%:*}
     shift 1
     local cmake_arg=$@
     echo "cmake arg: $cmake_arg"
 
-    echo "Pulling dockcross/$image:latest"
-    docker pull dockcross/$image:latest
+    echo "Pulling dockcross/$image"
+    docker pull dockcross/$image
     echo "Make script dockcross-$image"
-    docker run --rm dockcross/$image:latest > ./dockcross-$image
+    docker run --rm dockcross/$image > ./dockcross-$image
     chmod +x ./dockcross-$image
-    echo "Build build-$image"
-    ./dockcross-$image cmake -Bbuild-$image -H. -GNinja $cmake_arg
-    ./dockcross-$image ninja -Cbuild-$image
+    echo "Build $build_file"
+    ./dockcross-$image cmake -B$build_file -H. -GNinja $cmake_arg
+    ./dockcross-$image ninja -C$build_file
 }
--- builder/functions/makefile_fn.sh
+++ builder/functions/makefile_fn.sh
@@ -5,10 +5,10 @@ makefile_build () {
     local image=$1
     shift 1
 
-    echo "Pulling dockcross/$image:latest"
-    docker pull dockcross/$image:latest
+    echo "Pulling dockcross/$image"
+    docker pull dockcross/$image
     echo "Make script dockcross-$image"
-    docker run --rm dockcross/$image:latest > ./dockcross-$image
+    docker run --rm dockcross/$image > ./dockcross-$image
     chmod +x ./dockcross-$image
     echo "Build build-$image"
     ./dockcross-$image bash -c 'make CXX=$CXX CC=$CC AR=$AR AS=$AS CPP=$CPP FC=$FC'
