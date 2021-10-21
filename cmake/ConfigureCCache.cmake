9d3f0e970b3d47e779f2da9afd1fd22e25546842
Bensuperpc <bensuperpc@gmail.com>
2021-10-21 15:22:44 +0200

    Delete ConfigureCCache.cmake

--- cmake/ConfigureCCache.cmake
+++
@@ -1,7 +0,0 @@
-find_program(CCACHE_FOUND ccache)
-if(CCACHE_FOUND AND ENABLE_CCACHE)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
-    set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
-    set(CMAKE_C_COMPILER_LAUNCHER ccache)
-endif()
