5ac4fd7c374dae9ecc1ceb6559db78e5f47b1d9d
Bensuperpc <bensuperpc@gmail.com>
2021-10-22 11:42:17 +0200

    Fix codespell
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- cmake/spell.cmake
+++ cmake/spell.cmake
@@ -9,7 +9,7 @@ endmacro()
 default(SPELL_COMMAND codespell)
 default(FIX NO)
 
-set(flag "--ignore-words=codespell-ignore.txt")
+set(flag "--ignore-words codespell-ignore.txt")
 if(FIX)
   set(flag -w)
 endif()
