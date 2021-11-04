dd396f5de45cd0cba6e6faeead0ad7a41cc4778d
Bensuperpc <bensuperpc@gmail.com>
2021-11-04 22:15:35 +0100

    Improve tests
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

---
+++ test/source/find_string_inv_test.cpp
@@ -0,0 +1,82 @@
+#include <array>
+#include <string>
+
+#include "gta_sa_lib.hpp"
+#include "gtest/gtest.h"
+
+static const auto array_size = 29;
+
+TEST(jamcrc, basic1)
+{
+  std::array<char, array_size> tmp1 = {0};
+
+  gta::find_string_inv(tmp1.data(), 0);
+
+  EXPECT_NE("", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("B", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("C", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("AA", static_cast<std::string>(tmp1.data()));
+
+  EXPECT_EQ("A", static_cast<std::string>(tmp1.data()));
+}
+
+TEST(jamcrc, basic2)
+{
+  std::array<char, array_size> tmp1 = {0};
+
+  gta::find_string_inv(tmp1.data(), 255);
+
+  EXPECT_NE("", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("A", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("B", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("C", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("AA", static_cast<std::string>(tmp1.data()));
+
+  EXPECT_EQ("UI", static_cast<std::string>(tmp1.data()));
+}
+
+TEST(jamcrc, basic3)
+{
+  std::array<char, array_size> tmp1 = {0};
+
+  gta::find_string_inv(tmp1.data(), 40000);
+
+  EXPECT_NE("", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("A", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("B", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("C", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("AA", static_cast<std::string>(tmp1.data()));
+
+  EXPECT_EQ("LDGB", static_cast<std::string>(tmp1.data()));
+}
+
+TEST(jamcrc, basic4)
+{
+  std::array<char, array_size> tmp1 = {0};
+
+  gta::find_string_inv(tmp1.data(), 1000000000);
+
+  EXPECT_NE("", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("A", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("B", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("C", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("AA", static_cast<std::string>(tmp1.data()));
+
+  EXPECT_EQ("LXSGDFC", static_cast<std::string>(tmp1.data()));
+}
+
+TEST(jamcrc, basic5)
+{
+  std::array<char, array_size> tmp1 = {0};
+
+  gta::find_string_inv(tmp1.data(), 1000000000000);
+
+  EXPECT_NE("", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("A", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("B", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("C", static_cast<std::string>(tmp1.data()));
+  EXPECT_NE("AA", static_cast<std::string>(tmp1.data()));
+
+  EXPECT_EQ("NUXRHCMTD", static_cast<std::string>(tmp1.data()));
+}
--- test/source/jamcrc_test.cpp
+++ test/source/jamcrc_test.cpp
@@ -3,10 +3,80 @@
 #include "gta_sa_lib.hpp"
 #include "gtest/gtest.h"
 
-TEST(jamcrc, basic)
+TEST(jamcrc, basic1)
 {
   gta::precompute_crc();
 
-  std::string str = "AAGCEY";
-  EXPECT_EQ(0x40cf761, gta::jamcrc(str));
+  std::string str = "";
+  std::reverse(str.begin(), str.end());
+
+  EXPECT_EQ("", str);
+
+  EXPECT_NE(0x0, gta::jamcrc(str));
+  EXPECT_EQ(0xffffffff, gta::jamcrc(str));
+}
+
+TEST(jamcrc, basic2)
+{
+  gta::precompute_crc();
+
+  std::string str = "ASNAEB";
+  std::reverse(str.begin(), str.end());
+
+  EXPECT_EQ("BEANSA", str);
+
+  EXPECT_NE(0x0, gta::jamcrc(str));
+  EXPECT_EQ(0x555fc201, gta::jamcrc(str));
+}
+
+TEST(jamcrc, basic3)
+{
+  gta::precompute_crc();
+
+  std::string str = "ASBHGRB";
+  std::reverse(str.begin(), str.end());
+
+  EXPECT_EQ("BRGHBSA", str);
+
+  EXPECT_NE(0x0, gta::jamcrc(str));
+  EXPECT_EQ(0xa7613f99, gta::jamcrc(str));
+}
+
+TEST(jamcrc, basic4)
+{
+  gta::precompute_crc();
+
+  std::string str = "XICWMD";
+  std::reverse(str.begin(), str.end());
+
+  EXPECT_EQ("DMWCIX", str);
+
+  EXPECT_NE(0x0, gta::jamcrc(str));
+  EXPECT_EQ(0x1a9aa3d6, gta::jamcrc(str));
+}
+
+TEST(jamcrc, basic5)
+{
+  gta::precompute_crc();
+
+  std::string str = "LGBTQIA+";
+  std::reverse(str.begin(), str.end());
+
+  EXPECT_EQ("+AIQTBGL", str);
+
+  EXPECT_NE(0x0, gta::jamcrc(str));
+  EXPECT_EQ(0x6ba88a6, gta::jamcrc(str));
+}
+
+TEST(jamcrc, basic6)
+{
+  gta::precompute_crc();
+
+  std::string str = "intergouvernementalisations";
+  std::reverse(str.begin(), str.end());
+
+  EXPECT_EQ("snoitasilatnemenrevuogretni", str);
+
+  EXPECT_NE(0x0, gta::jamcrc(str));
+  EXPECT_EQ(0x1a384955, gta::jamcrc(str));
 }
