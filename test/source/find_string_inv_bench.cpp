079ac8bc91816f91ae8fd3f0cf3242a98c400c2c
Bensuperpc <bensuperpc@gmail.com>
2021-11-09 19:15:01 +0100

    Improve bench
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- test/source/find_string_inv_bench.cpp
+++ test/source/find_string_inv_bench.cpp
@@ -13,20 +13,22 @@ static void find_string_inv_bench(benchmark::State& state)
   auto range = state.range(0);
 
   const auto array_size = 29;
-  std::array<char, array_size> tmp1 = {0};
+  std::array<char, array_size> tmp = {0};
 
   for (auto _ : state) {
-    gta::find_string_inv(tmp1.data(), range);
-    // Make sure the variable is not optimized away by compiler
-    // benchmark::DoNotOptimize(tmp1);
+    gta::find_string_inv(tmp.data(), range);
+    benchmark::DoNotOptimize(tmp);
+    // benchmark::ClobberMemory();
   }
+  state.SetItemsProcessed(state.iterations());
+  // state.SetBytesProcessed(state.iterations() * state.range(0) *
+  // sizeof(char));
 }
 // Register the function as a benchmark
-BENCHMARK(find_string_inv_bench)->Arg(10);
-BENCHMARK(find_string_inv_bench)->Arg(100);
-BENCHMARK(find_string_inv_bench)->Arg(1000);
-BENCHMARK(find_string_inv_bench)->Arg(1000000);
-BENCHMARK(find_string_inv_bench)->Arg(1000000000);
+BENCHMARK(find_string_inv_bench)
+    ->Name("find_string_inv")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000000000);
 
 // Run the benchmark
-BENCHMARK_MAIN();
+// BENCHMARK_MAIN();
--- test/source/jamcrc_bench.cpp
+++ test/source/jamcrc_bench.cpp
@@ -1,25 +1,56 @@
 #include <array>
+#include <memory>
 #include <string>
-#include <string_view>
 #include <vector>
 
 #include <benchmark/benchmark.h>
 
 #include "gta_sa_lib.hpp"
 
+// Source: https://github.com/stbrumme/crc32
+static std::unique_ptr<char[]> generate(const std::uint64_t length);
+
+static std::unique_ptr<char[]> generate(const std::uint64_t length)
+{
+  uint32_t randomNumber = 0x27121978;
+  // initialize
+  std::unique_ptr<char[]> data(new char[length]);
+  for (size_t i = 0; i < length; i++) {
+    data[i] = char(randomNumber & 0xFF);
+    // simple LCG, see
+    // http://en.wikipedia.org/wiki/Linear_congruential_generator
+    randomNumber = 1664525 * randomNumber + 1013904223;
+  }
+  return data;
+}
+
 static void jamcrc_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
-  std::string created_string("01234567890123456789");
+  auto size = state.range(0);
+  auto c_str = generate(size);
+
+  std::string str;
+  str.reserve();
+  for (size_t i = 0; i < size; i++) {
+    str.push_back(c_str[i] + '0');
+  }
+  c_str.release();
 
   for (auto _ : state) {
-    gta::jamcrc(created_string);
-    // Make sure the variable is not optimized away by compiler
-    benchmark::DoNotOptimize(created_string);
+    benchmark::DoNotOptimize(str);
+    gta::jamcrc(str);
+    benchmark::ClobberMemory();
   }
+  // state.SetItemsProcessed(state.iterations() * state.range(0));
+  state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+
+  // state.SetLabel("OK");
 }
-// Register the function as a benchmark
-BENCHMARK(jamcrc_bench);
+BENCHMARK(jamcrc_bench)
+    ->Name("jamcrc")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
 // Run the benchmark
-BENCHMARK_MAIN();
+// BENCHMARK_MAIN();
