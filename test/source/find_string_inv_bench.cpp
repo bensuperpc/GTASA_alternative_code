5a07e2b6cf7c190dce196b2acb1df789ae88f84a
Bensuperpc <bensuperpc@gmail.com>
2022-03-07 13:06:09 +0100

    Update CMake-init to 0.26.2, commit N°3
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

---
+++ test/source/find_string_inv_bench.cpp
@@ -0,0 +1,34 @@
+#include <array>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <benchmark/benchmark.h>
+
+#include "gta_sa_lib.hpp"
+
+static void find_string_inv_bench(benchmark::State& state)
+{
+  // Code inside this loop is measured repeatedly
+  auto range = state.range(0);
+
+  const auto array_size = 29;
+  std::array<char, array_size> tmp = {0};
+
+  for (auto _ : state) {
+    gta::find_string_inv(tmp.data(), range);
+    benchmark::DoNotOptimize(tmp);
+    // benchmark::ClobberMemory();
+  }
+  state.SetItemsProcessed(state.iterations());
+  // state.SetBytesProcessed(state.iterations() * state.range(0) *
+  // sizeof(char));
+}
+// Register the function as a benchmark
+BENCHMARK(find_string_inv_bench)
+    ->Name("find_string_inv")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000000000);
+
+// Run the benchmark
+// BENCHMARK_MAIN();
