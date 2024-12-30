b6be2dd8e71667ab9bbcaa38edb49e2348b76931
Bensuperpc <bensuperpc@gmail.com>
2024-12-30 20:06:56 +0100

    Fixes for macs CI
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,6 +1,11 @@
 install(
     TARGETS GTASA_alternative_code_exe
     RUNTIME COMPONENT GTASA_alternative_code_Runtime
+    BUNDLE DESTINATION bin
+#    LIBRARY DESTINATION lib${LIB_SUFFIX}
+#    ARCHIVE DESTINATION lib${LIB_SUFFIX}
+#    RUNTIME DESTINATION bin
+#    BUNDLE DESTINATION bin
 )
 
 if(PROJECT_IS_TOP_LEVEL)
